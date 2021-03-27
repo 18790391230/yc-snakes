ea126ebfa0f1b32fd9c9d141767271296811f7ba
18790391230 <751586981@qq.com>
2021-03-27 17:24:45 +0800

    4

--- src/main/java/com/yc/snacks/service/UserService.java
+++ src/main/java/com/yc/snacks/service/UserService.java
@@ -1,6 +1,10 @@
 package com.yc.snacks.service;
 
+import java.util.List;
+
 public interface UserService {
 
     boolean firstLogin(int empId);
+
+    void saveUserTags(List<String> tags);
 }
--- src/main/java/com/yc/snacks/service/impl/UserServiceImpl.java
+++ src/main/java/com/yc/snacks/service/impl/UserServiceImpl.java
@@ -6,6 +6,8 @@ import com.yc.snacks.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -18,5 +20,12 @@ public class UserServiceImpl implements UserService {
         EmpGroup empGroup = empGroupMapper.selectByEmpId(empId);
 
         return empGroup.getFirstLogin() != 1;
+    }
+
+    @Override
+    public void saveUserTags(List<String> tags) {
+
+
+//        empGroupMapper.insertSelective()
     }
 }
